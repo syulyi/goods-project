0b7b156d18c84367659685e6e4f939e392b91e6d
syulyi <hghghgfvdi@gmail.com>
2023-12-11 06:27:50 +0900

    12-11(3). board_nav.css 추가, settings.py 에서 css 적용 추가

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# css 적용
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
